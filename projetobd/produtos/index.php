7702ea8d14bb355ec4e0a1a62595b06cafd7dc0a
alicegdutra <alicegzdutra@gmail.com>
2024-05-11 10:25:26 -0300

    commit

--- projetobd/produtos/index.php
+++ projetobd/produtos/index.php
@@ -3,6 +3,40 @@
 ?>
     <h1>Gerenciamento de Produtos</h1>
 
+    <a href="" class="btn btn-primary mt-3">Adicionar Produto</a>
+
+    <table class="mt-3 table table-hover table-striped">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+                <th>Categoria</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Persuasão</td>
+                <td>Livro de Jane Austen</td>
+                <td>R$10,00</td>
+                <td>Romance</td>
+            </tr>
+            <tr>
+                <td>Um corpo na Biblioteca</td>
+                <td>Livro de Agatha Christie</td>
+                <td>R$20,00</td>
+                <td>Mistério</td>
+            </tr>
+            <tr>
+                <td>Hobbit</td>
+                <td>Livro de J. R. R. Tolkien</td>
+                <td>R$50,00</td>
+                <td>Fantasia</td>
+            </tr>
+        </tbody>
+
+    </table>
+
 <?php
     require_once("../rodape2.html");
 ?>
